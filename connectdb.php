6fc9ea88ef45a266ea55e67d8206d6762674ceaf
Mario Zelaya <mariozelaya23@gmail.com>
2022-01-30 13:18:17 -0800

    adding try catch to connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,5 +1,9 @@
-<?php 
-    $pdo = new PDO('mysql:host=localhost;dbname=clinic_db','root','');
-    echo 'connection successfull'
-
+<?php
+    try{
+        $pdo = new PDO('mysql:host=localhost;dbname=clinic_db','root','');
+        echo 'connection successfull';
+    }catch(PDOException $f){
+        echo $f->getMessage();
+    }
+    
 ?>
