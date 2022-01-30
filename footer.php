2b86fc5cdf764b1be0462f71f8ffce1e24ab3c14
Mario Zelaya <mariozelaya23@gmail.com>
2022-01-30 11:23:24 -0800

    adding footer.php, header.php, blank.php, and just letting the necessary information on dashboard.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Sistema para la administración de clinica - V-1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>BY: <a href="#">MZ</a></strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
