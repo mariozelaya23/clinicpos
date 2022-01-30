e09384e71c295e29c1b558102cfa11d5e22d2db7
Mario Zelaya <mariozelaya23@gmail.com>
2022-01-30 15:46:42 -0800

    logout.php added, on user.php and dashboard.php I added username and role information from the database

--- header.php
+++ header.php
@@ -72,7 +72,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?php echo $_SESSION['username']." (".$_SESSION['role'].")";?></a> <!-- showing admin username from the database -->
         </div>
       </div>
 
@@ -135,7 +135,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="logout.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Logout
