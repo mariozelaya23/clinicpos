1b63cd5b60d113c542bbf8fda21ddae267e00325
Mario Zelaya <mariozelaya23@gmail.com>
2022-02-07 20:47:59 -0800

    fixing frontend section on changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -107,53 +107,60 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Actualizar contraseña</h1>
-          </div><!-- /.col -->
+            <h1>Actualizar contraseña</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Actualizar contraseña</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    <div class="card card-primary">
-      <div class="card-header">
-        <h3 class="card-title">Formulario para la actualización de contraseña</h3>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+    
+      <div class="card card-primary">
+        <div class="card-header">
+          <h3 class="card-title">Formulario para la actualización de contraseña</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form action="" method="POST">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="exampleInputPassword1">Contraseña antigua</label>
+              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_oldpass" required>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="exampleInputPassword1">Nueva contraseña</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_newpass" required>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="exampleInputPassword1">Confirmar contraseña</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_confpass" required>
+            </div>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary" name="btn_update">Actualizar</button>
+          </div>
+        </form>
       </div>
-      <!-- /.card-header -->
-      <!-- form start -->
-      <form action="" method="POST">
-        <div class="card-body">
-          <div class="form-group">
-            <label for="exampleInputPassword1">Contraseña antigua</label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_oldpass" required>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="form-group">
-            <label for="exampleInputPassword1">Nueva contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_newpass" required>
-          </div>
-        </div>
-        <div class="card-body">
-          <div class="form-group">
-            <label for="exampleInputPassword1">Confirmar contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txt_confpass" required>
-          </div>
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          <button type="submit" class="btn btn-primary" name="btn_update">Actualizar</button>
-        </div>
-      </form>
-    </div>
+
+
+    </section>
+    <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
