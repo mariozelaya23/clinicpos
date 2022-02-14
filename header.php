33ecd6bd24853d12f0f4306e79b10fe7dee3af8d
Mario Zelaya <mariozelaya23@gmail.com>
2022-02-13 17:17:15 -0800

    fixing navvar on header.php

--- header.php
+++ header.php
@@ -117,31 +117,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <!-- <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Starter Pages
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
-          </li> -->
           <li class="nav-item">
             <a href="dashboard.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -150,23 +125,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-hospital-user"></i>
               <p>
                 Pacientes
-                <i class="right fas fa-angle-left"></i>
+                <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./addpatient.php" class="nav-link">
-                <i class="fas fa-folder-plus nav-icon"></i>
+                <a href="addpatient.php" class="nav-link">
+                  <i class="fas fa-folder-plus nav-icon"></i>
                   <p>Agregar Paciente</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./patientlist.php" class="nav-link">
+                <a href="patientlist.php" class="nav-link">
                   <i class="fas fa-list-alt nav-icon"></i>
                   <p>Lista de Pacientes</p>
                 </a>
