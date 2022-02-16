ebd810edd2d2a076b182e70eba770be53b7a1694
Mario Zelaya <mariozelaya23@gmail.com>
2022-02-15 16:24:27 -0800

    on patientlist.php adding code for patient table, and adding view, edit, and delete buttons

--- uregistration.php
+++ uregistration.php
@@ -363,24 +363,6 @@
   } );
 </script>
 
-<!-- <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-</script> -->
-
 <?php
   include_once 'footer.php';
 ?>
